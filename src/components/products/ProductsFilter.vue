b11eae023c8dc5e4529f2347d3613f83406a60ac
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2023-10-09 10:08:20 +0300

    products filter: - fixed hardcode

--- src/components/products/ProductsFilter.vue
+++ src/components/products/ProductsFilter.vue
@@ -2,19 +2,19 @@
   <div class="filter">
     <div class="filter__inner">
       <base-filter
-        v-model:selected="filterCategory"
+        v-model:selected="activeFilters.category"
         :elements="categories"
         name="category"
         class="filter__block"
       />
       <base-filter
-        v-model:selected="filterBrand"
+        v-model:selected="activeFilters.brand"
         :elements="brands"
         name="brand"
         class="filter__block"
       />
       <base-input
-        v-model:value="filterSearch"
+        v-model:value="activeFilters.search"
         type="search"
         title="Search by title"
         placeholder="Type something"
@@ -25,7 +25,8 @@
           <base-input
             :min-value="minimumPrice"
             :max-value="maximumPrice"
-            v-model:value="minPrice"
+            :placeholder="String(minimumPrice)"
+            v-model:value="activeFilters.minPrice"
             type="number"
             title="Minimum price"
             class="price__input"
@@ -33,7 +34,8 @@
           <base-input
             :min-value="minimumPrice"
             :max-value="maximumPrice"
-            v-model:value="maxPrice"
+            :placeholder="String(maximumPrice)"
+            v-model:value="activeFilters.maxPrice"
             type="number"
             title="Maximum price"
             class="price__input"
@@ -51,7 +53,7 @@
 
 <script setup>
   import BaseFilter from '@/components/base/BaseFilter.vue';
-  import {computed, ref, watch} from 'vue';
+  import {computed, reactive, ref, watch} from 'vue';
   import BaseButton from '@/components/base/BaseButton.vue';
   import BaseInput from '@/components/base/BaseInput.vue';
 
@@ -66,13 +68,55 @@
   const products = computed(() => props.products);
   const categories = ref([]);
   const brands = ref([]);
-  const filterCategory = ref('all');
-  const filterBrand = ref('all');
-  const filterSearch = ref('');
-  const minimumPrice = ref(0);
-  const maximumPrice = ref(0);
-  const minPrice = ref(null);
-  const maxPrice = ref(null);
+  const minimumPrice = ref(null);
+  const maximumPrice = ref(null);
+  const filteredProducts = ref(products.value);
+
+  const activeFilters = reactive({
+    category: 'all',
+    brand: 'all',
+    search: '',
+    minPrice: null,
+    maxPrice: null,
+  });
+
+  const filters = [
+    {
+      key: 'category',
+      value: ref(''),
+      method: function (product) {
+        return this.value === 'all' ? product.category.matchAll(`A-Za-z0-9_-/`) : product.category === this.value;
+      },
+    },
+    {
+      key: 'brand',
+      value: ref(''),
+      method: function (product) {
+        return this.value === 'all' ? product.brand.matchAll(`A-Za-z0-9_-/`) : product.brand === this.value;
+      },
+    },
+    {
+      key: 'search',
+      value: ref(''),
+      method: function (product) {
+        return product.title.toLowerCase().includes(this.value.toLowerCase());
+      },
+    },
+    {
+      key: 'minPrice',
+      value: ref(0),
+      method: function (product) {
+        return this.value ? product.price >= Number(this.value) : true;
+      },
+    },
+    {
+      key: 'maxPrice',
+      value: ref(0),
+      method: function (product) {
+        return this.value ? product.price <= Number(this.value) : true;
+      },
+    },
+  ];
 
   const getMinimumPrice = () => {
     return products.value.reduce((min, p) => (p.price < min ? p.price : min), products.value[0].price);
@@ -86,52 +130,37 @@
   minimumPrice.value = getMinimumPrice();
   maximumPrice.value = getMaximumPrice();
 
-  watch(filterCategory, () => {
-    filterProducts();
-  });
+  const includeAllFilters = (product) => {
+    let mapFilterMethods = filters.map((filter) => filter.method(product));
+    if (allAreTrue(mapFilterMethods)) {
+      return true;
+    }
+  };
 
-  watch(filterBrand, () => {
-    filterProducts();
-  });
+  const allAreTrue = (arr) => arr.every((element) => element !== false);
 
-  watch(filterSearch, () => {
-    filterProducts();
-  });
+  watch(activeFilters, (values) => {
+    Object.entries(values).forEach((value) => {
+      let activeFilter = filters.find((element) => element.key === value[0]);
+      activeFilter.value = value[1];
+    });
 
-  watch(minPrice, () => {
-    filterProducts();
-  });
+    filteredProducts.value = products.value.filter((product) => {
+      if (includeAllFilters(product)) {
+        return product;
+      }
+    });
 
-  watch(maxPrice, () => {
-    filterProducts();
+    emit('filters', filteredProducts.value);
   });
 
   const resetFilters = () => {
-    filterCategory.value = 'all';
-    filterBrand.value = 'all';
-    filterSearch.value = '';
-    minPrice.value = '';
-    maxPrice.value = '';
+    activeFilters.category = 'all';
+    activeFilters.brand = 'all';
+    activeFilters.search = '';
+    activeFilters.minPrice = null;
+    activeFilters.maxPrice = null;
     emit('filters', 'reset');
-  };
-
-  const filterProducts = () => {
-    const category = filterCategory.value;
-    const brand = filterBrand.value;
-    const search = filterSearch.value;
-    const min = minPrice.value;
-    const max = maxPrice.value;
-    const regexp = `A-Za-z0-9_-/`;
-
-    const productsList = products.value.filter(
-      (product) =>
-        (brand === 'all' ? product.brand.matchAll(regexp) : product.brand === brand) &&
-        (category === 'all' ? product.category.matchAll(regexp) : product.category === category) &&
-        product.title.toLowerCase().includes(search.toLowerCase()) &&
-        (min ? product.price >= Number(min) : true) &&
-        (max ? product.price <= Number(max) : true),
-    );
-    emit('filters', productsList);
   };
 </script>
 
